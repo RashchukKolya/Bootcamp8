451eee85a50d1e946ad67d5be585f65f610dccac
Nick <raschyk1993@gmail.com>
2018-11-20 10:22:35 +0200

    js

--- javascript-homework/Module-8/JS/main.js
+++ javascript-homework/Module-8/JS/main.js
@@ -1,19 +1,19 @@
 'use strict'
 
 const galleryItems = [
-  {preview: '../IMG/Preview/preview-1.jpeg', fullview: '../IMG/Fullview/fullview-1.jpeg', alt: "alt text 1"},
-  {preview: '../IMG/Preview/preview-2.jpeg', fullview: '../IMG/Fullview/fullview-2.jpeg', alt: "alt text 2"},
-  {preview: '../IMG/Preview/preview-3.jpeg', fullview: '../IMG/Fullview/fullview-3.jpeg', alt: "alt text 3"},
-  {preview: '../IMG/Preview/preview-4.jpeg', fullview: '../IMG/Fullview/fullview-4.jpeg', alt: "alt text 4"},
-  {preview: '../IMG/Preview/preview-5.jpeg', fullview: '../IMG/Fullview/fullview-5.jpeg', alt: "alt text 5"},
-  {preview: '../IMG/Preview/preview-6.jpeg', fullview: '../IMG/Fullview/fullview-6.jpeg', alt: "alt text 6"},
-  {preview: '../IMG/Preview/preview-7.jpeg', fullview: '../IMG/Fullview/fullview-7.jpeg', alt: "alt text 7"},
-  {preview: '../IMG/Preview/preview-8.jpeg', fullview: '../IMG/Fullview/fullview-8.jpeg', alt: "alt text 8"},
-  {preview: '../IMG/Preview/preview-9.jpeg', fullview: '../IMG/Fullview/fullview-9.jpeg', alt: "alt text 9"},
-  {preview: '../IMG/Preview/preview-10.jpeg', fullview: '../IMG/Fullview/fullview-10.jpeg', alt: "alt text 10"},
-  {preview: '../IMG/Preview/preview-11.jpeg', fullview: '../IMG/Fullview/fullview-11.jpeg', alt: "alt text 11"},
-  {preview: '../IMG/Preview/preview-12.jpeg', fullview: '../IMG/Fullview/fullview-12.jpeg', alt: "alt text 12"},
-  {preview: '../IMG/Preview/preview-13.jpeg', fullview: '../IMG/Fullview/fullview-13.jpeg', alt: "alt text 13"},
+  {preview: './IMG/Preview/preview-1.jpeg', fullview: './IMG/Fullview/fullview-1.jpeg', alt: "alt text 1"},
+  {preview: './IMG/Preview/preview-2.jpeg', fullview: './IMG/Fullview/fullview-2.jpeg', alt: "alt text 2"},
+  {preview: './IMG/Preview/preview-3.jpeg', fullview: './IMG/Fullview/fullview-3.jpeg', alt: "alt text 3"},
+  {preview: './IMG/Preview/preview-4.jpeg', fullview: './IMG/Fullview/fullview-4.jpeg', alt: "alt text 4"},
+  {preview: './IMG/Preview/preview-5.jpeg', fullview: './IMG/Fullview/fullview-5.jpeg', alt: "alt text 5"},
+  {preview: './IMG/Preview/preview-6.jpeg', fullview: './IMG/Fullview/fullview-6.jpeg', alt: "alt text 6"},
+  {preview: './IMG/Preview/preview-7.jpeg', fullview: './IMG/Fullview/fullview-7.jpeg', alt: "alt text 7"},
+  {preview: './IMG/Preview/preview-8.jpeg', fullview: './IMG/Fullview/fullview-8.jpeg', alt: "alt text 8"},
+  {preview: './IMG/Preview/preview-9.jpeg', fullview: './IMG/Fullview/fullview-9.jpeg', alt: "alt text 9"},
+  {preview: './IMG/Preview/preview-10.jpeg', fullview: './IMG/Fullview/fullview-10.jpeg', alt: "alt text 10"},
+  {preview: './IMG/Preview/preview-11.jpeg', fullview: './IMG/Fullview/fullview-11.jpeg', alt: "alt text 11"},
+  {preview: './IMG/Preview/preview-12.jpeg', fullview: './IMG/Fullview/fullview-12.jpeg', alt: "alt text 12"},
+  {preview: './IMG/Preview/preview-13.jpeg', fullview: './IMG/Fullview/fullview-13.jpeg', alt: "alt text 13"},
 ];
 
 const body = document.querySelector('body');
